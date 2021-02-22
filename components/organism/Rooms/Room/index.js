6b5cd48c7cc44425fa90cbae764cc8db6b576196
Leonardo T <easaaa@protonamail.com>
2021-02-22 16:46:16 +0100

    Fix order rooms index page

--- components/organism/Rooms/Room/index.js
+++ components/organism/Rooms/Room/index.js
@@ -5,7 +5,8 @@ export default function Room({ room: { name, img, price }, index }) {
   return (
     <section class='lg:py-12 lg:flex lg:justify-center'>
       <div class='lg:mx-8 lg:flex lg:max-w-5xl rounded-lg'>
-        <div class={`lg:w-1/2`}>
+        <div
+          class={`lg:w-1/2 ${(index + 1) % 2 === 0 && 'order-2  md:order-1'}`}>
           <div
             class='h-64 bg-cover rounded-lg lg:h-full'
             style={{
@@ -40,27 +41,3 @@ export default function Room({ room: { name, img, price }, index }) {
     </section>
   );
 }
-/* 
-<div className='p-4'>
-<div
-  className='flex flex-col relative place-items-center cursor-pointer w-96 rounded-md transform hover:scale-105 hover:underline duration-100 ease-in-out'
-  onClick={() =>
-    router
-      .push({ pathname: '/appartamenti', query: { room: name } })
-      .then(() => window.scrollTo(0, 0))
-  }>
-  <img
-    alt='gallery'
-    className='inset-0 w-full h-80 object-cover object-center rounded-md shadow-md hover:shadow-lg'
-    src={img}
-  />
-  <div className='text-center'>
-    <h1 className='text-xl font-bold text-green-900 mt-2 capitalize font-serif '>
-      {name}
-    </h1>
-    <p className='leading-relaxed'>{price}€ per Night</p>
-  </div>
-</div>
-
-${(index + 1) % 2 === 0 && 'order-2  md:order-1'}
-</div> */
