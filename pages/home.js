52b884855a7ac1d12966eb4fc9fc68b585529579
leooo.eth <easaaa@protonmail.com>
2022-07-24 11:27:58 +0200

    fix: removed terra usd

--- pages/home.js
+++ pages/home.js
@@ -38,22 +38,17 @@ export default function IndexPage() {
             We also accept cryptocurrencies.
           </h4>
           <div class="flex flex-wrap -mx-4 items-center">
-            <div class="w-full w-1/2 lg:w-1/4 px-4 text-center mb-10 lg:mb-0">
+            <div class="w-1/2 lg:w-1/3 px-4 text-center mb-10 lg:mb-0">
               <div class="p-4 h-40 flex items-center justify-center bg-orange-100">
                 <img src="/assets/crypto/bitcoin.png" alt="" class="w-10" />
               </div>
             </div>
-            <div class="w-full w-1/2 lg:w-1/4 px-4 text-center mb-10 lg:mb-0">
+            <div class="w-1/2 lg:w-1/3 px-4 text-center mb-10 lg:mb-0">
               <div class="p-4 h-40 flex items-center justify-center bg-gray-100">
                 <img src="/assets/crypto/ethereum.png" alt="" class="w-10" />
               </div>
             </div>
-            <div class="w-full w-1/2 lg:w-1/4 px-4 text-center mb-10 md:mb-0">
-              <div class="p-4 h-40 flex items-center justify-center bg-blue-100">
-                <img src="/assets/crypto/terra-usd.png" alt="" class="w-10" />
-              </div>
-            </div>
-            <div class="w-full w-1/2 lg:w-1/4 px-4 text-center mb-10 md:mb-0">
+            <div class="w-full lg:w-1/3 px-4 text-center mb-10 md:mb-0">
               <div class="p-4 h-40 flex items-center justify-center bg-yellow-100">
                 <img src="/assets/crypto/busd.png" alt="" class="w-10" />
               </div>
