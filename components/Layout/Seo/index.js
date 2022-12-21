bb30c0049ba18dbfeca2a72e17a4650877f22930
LeonardoTononi <easaaa@protonmail.com>
2022-12-21 16:44:42 -0300

    add favicon

--- components/Layout/Seo/index.js
+++ components/Layout/Seo/index.js
@@ -42,6 +42,26 @@ export default function Seo({ description, title, keywords }) {
         property="og:url"
         content={`https://agriturismosanlorenzodipersegno.it/${router?.asPath}`}
       />
+
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+
       {/*  <meta name='twitter:card' content='summary_large_image' />
       <meta name='twitter:creator' content='@timsy.io' />
       <meta name='twitter:title' content={`Timsy — ${title}`} />
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,17 @@
 import Layout from "../components/Layout";
+import Head from "next/head";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout className="antialiased bg-white">
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
+      <Layout className="antialiased bg-white">
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
